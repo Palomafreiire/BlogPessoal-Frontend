2d2de0e9031231945e542e89ade7f8153f1e1e47
Paloma <palomafreiire@hotmail.com>
2022-09-09 11:25:45 -0300

    CRUD tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -22,8 +22,20 @@ export class TemaService {
         return this.http.get<tema[]>('http://localhost:8080/tema', this.token)
     }
 
+    getByIdTema(id:number): Observable<tema>{  //mesma coisa que o delete, não precisa de objeto pq vai ser por id (igual ta no back)
+        return this.http.get<tema>(`http://localhost:8080/tema/${id}`, this.token)
+    }
+
     postTema(tema: tema): Observable<tema>{ // não~bota o array[] pq vai ser um tema por vez
       return this.http.post<tema>('http://localhost:8080/tema', tema, this.token)
+    }
+
+    putTema(tema: tema): Observable<tema>{
+      return this.http.put<tema>('http://localhost:8080/tema', tema, this.token)
+    }
+
+    deleteTema (id:number){  // em delete não precisa de um observable pq não precisa de um objeto, no back faz todo o trbalho;
+      return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)    //tem que abrir uma crase ` para o delete
     }
 
 
